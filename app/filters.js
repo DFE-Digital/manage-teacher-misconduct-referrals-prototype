f4dfa77b2ffaa31a42d81761d4199465ceb7d253
Adam Silver <adambsilver@gmail.com>
2022-12-20 15:13:47 +0000

    Add date time to both list and details pages

--- app/filters.js
+++ app/filters.js
@@ -2,6 +2,23 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 const { DateTime } = require("luxon")
 
-addFilter('date', function(date) {
+function govukDate(date) {
   return DateTime.fromISO(date).toFormat('d MMMM yyyy')
-})
+}
+addFilter('date', govukDate)
+
+function govukTime(date) {
+  let dt = DateTime.fromISO(date)
+  if (dt.minute > 0) {
+    dt = dt.toFormat('h:mma')
+  } else {
+    dt = dt.toFormat('ha')
+  }
+  return dt.toLowerCase()
+}
+addFilter('time', govukTime)
+
+function govukDateTime(date) {
+  return govukDate(date) + ' at ' + govukTime(date)
+}
+addFilter('dateTime', govukDateTime)
