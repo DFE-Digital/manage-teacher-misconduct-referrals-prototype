6631386ce2d910cd66d9dcdd505195d2b02cce89
Adam Silver <adambsilver@gmail.com>
2023-01-27 09:42:28 +0000

    Add super basic auth

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,16 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+router.post('/account/sign-in', (req, res) => {
+  req.session.data.user = {}
+  res.redirect('/referrals')
+})
+
+router.get('/account/sign-out', (req, res) => {
+  req.session.data.user = null
+  res.redirect('/')
+})
+
 router.get('/referrals', (req, res) => {
   let referrals = req.session.data.referrals.sort((a, b) => {
     return new Date(b.referralDate) - new Date(a.referralDate);
